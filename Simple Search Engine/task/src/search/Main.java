b96d7e2a3735b7f685b43afaf5ddaf0f163b8550
dev-otaher <dev.omar.taher@gmail.com>
2022-06-12 22:05:49 +0300

    Stage 1/6

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -7,9 +7,9 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String word = scanner.nextLine();
-        System.out.println(input.length());
         if (input.contains(word)) {
-            System.out.println(input.indexOf(word));
+            String substring = input.substring(0, input.indexOf(word) + word.length());
+            System.out.println(substring.split(" ").length);
         } else {
             System.out.println("Not found");
         }
